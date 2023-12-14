24739273dedc20d8367eab77ee56d8497ceb3958
Dídac <107183107+dtarragol@users.noreply.github.com>
2023-12-14 21:29:39 +0100

    primer commit

--- app/src/main/java/com/jose/diceroller/db/ApiClient.java
+++ app/src/main/java/com/jose/diceroller/db/ApiClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    private static final String BASE_URL = "https://dice-roller-97246-default-rtdb.firebaseio.com/";//url de console firebase RealTime
+    private static final String BASE_URL = "https://diceroller-d854f-default-rtdb.firebaseio.com/";//url de console firebase RealTime
     private static Retrofit retrofit = null;
     public static Retrofit getClient(){
         if (retrofit == null){
